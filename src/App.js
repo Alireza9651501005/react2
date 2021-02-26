351ca604b31109d72509c5a79f566ffa9ceef951
Alireza <Alireza691111@gmail.com>
2021-02-26 09:31:03 -0800

    title and back color changed

--- src/App.js
+++ src/App.js
@@ -6,18 +6,10 @@ const api = {
 }
 
 const App = () => {
-  // constructor() {
-  //   super()
-  //   this.state = {
-  //     mlist: []
-  //   }
-  // }
-  // const [State1, setState1] = useState([])
   let fetchData = () => {
     axios.get(`https://api.openweathermap.org/data/2.5/weather?q=Marbella&appid=${api.key}`)
       .then((res) => {
         console.log(res)
-        // setState1(res)
       })
       .catch(err => console.log('An Error has been ocuerred'))
   }
@@ -33,11 +25,6 @@ const App = () => {
             />
             <button onClick = {fetchData}>fetch</button>
             <h1>This is My List</h1>
-            {/* {
-              State1.map(row => {
-                return <p key = {row.id}>{row.title}</p>
-              })
-            } */}
           </div>
         </main>
       </div>
--- src/weather1/Title.js
+++ src/weather1/Title.js
@@ -4,7 +4,7 @@ class Title extends React.Component {
     render () {
         return (
             <div className= 'title'>
-                <h1> ولی بیست گرفت!</h1>
+                <h1>پیش بینی وضعیت آب و هوای شهرهای مختلف دنیا</h1>
                 <p>در کادر زیر شهر و کشور مورد نظر را وارد کنید</p>
             </div>
         )
